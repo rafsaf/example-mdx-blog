5217d799b3458b92487d6f5dc6d5bb859b666562
rafsaf <rafal.safin12@gmail.com>
2021-03-26 21:31:13 +0100

    added some navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import Links from '../constants/links'
 import Categories from '../components/Categories'
 import { IoMdClose } from 'react-icons/io'
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  return <h2></h2>
 }
 
 export default Sidebar
